5252c72208370cdb845cd2f3c8f3b7aa572dc077
Gungoguma <55743192+Gungoguma@users.noreply.github.com>
2021-04-01 15:58:43 +0900

    Todo 수정

--- src/Component/Todo.jsx
+++ src/Component/Todo.jsx
@@ -31,16 +31,30 @@ const Todo = () => {
         setNum(num + 1);
     }
 
-    const Show = ({list, onRemove}) => {
+    const Show = ({list, onRemove, onFix}) => {
         return(
             <>
                 <div style={{backgroundColor: "gray", color: "pink"}}>
                     <div>{list.title} <br/> {list.content} </div>
                     <button onClick={() => onRemove(list.id)}>삭제</button>
+                    <button onClick={() => onFix(list.id)}>수정</button>
                 </div>
                 <br/>
             </>
         );
+    }
+
+    const onFix = (id) => {
+        const temp = prompt();
+        
+        setList(list.map(n => 
+            n.id === id ? {
+                ...n,
+                content : temp
+            } : {...n}
+        )
+    )
+        
     }
 
     const onRemove = (id) => {
@@ -59,7 +73,7 @@ const Todo = () => {
             <br/>
             <div>
             {list.map(list => (
-                <Show list={list} key={list.id} onRemove={onRemove}/>
+                <Show list={list} key={list.id} onRemove={onRemove} onFix={onFix}/>
             ))}
             </div>
         </div>
