43d40c60501be92519545680b53a03604d444b21
Gungoguma <khh9550@naver.com>
2021-04-12 21:32:33 +0900

    UseReducer 완료

--- src/Component/UseReducer.jsx
+++ src/Component/UseReducer.jsx
@@ -2,73 +2,68 @@ import React, { useReducer, useRef } from 'react';
 
 function reducer(state, action) {
     switch (action.type) {
-        case 'WRITE_TODO':
+        case "WRITE_TODO":
             return {
-                 ...state,
+                ...state,
                 inputs: {
                     ...state.inputs,
                     [action.name]: action.value
                 }
-            };
-        case 'ADD_TODO':
+            }
+        case "ADD_TODO":
             return {
-                inputs: '',
-                todos: [
-                    ...state.todos,
+                inputs: state.inputs,
+                list: [
+                    ...state.list,
                     {
-                        id: action.id,
                         title: action.title,
-                        content: action.content
+                        content: action.content,
+                        id: action.id
                     }
+
                 ]
             }
-        case 'DELETE_TODO':
+        case "DELETE_TODO":
             return {
-                todos : state.todos.filter(x => x.id !== action.id)
+                list: state.list.filter(x => x.id !== action.id)
             }
-        case 'FIX_TODO':
+        case "FIX_TODO":
             return {
-                todos : state.todos.map(x =>
-                    x.id === action.id ?
+                list: state.list.map(x => x.id === action.id ?
                     {
                         ...x,
-                        content : action.temp
-                    } :
-                    {...x}
-                )
+                        content: action.temp
+                    } : { ...x })
             }
-    };
-};
+    }
+}
 
-
-const initialState = {
+const init = {
     inputs: {
         title: '',
         content: ''
     },
-    todos: [
+    list: [
 
     ]
-};
+}
 
 const UseReducer = () => {
-
-    const [todoList, dispatch] = useReducer(reducer, initialState);
-    const { todos } = todoList;
+    const [todoList, dispatch] = useReducer(reducer, init);
     const nextId = useRef(0);
 
-    const onChangeBox = (e) => {
+    const onChangeTyping = (e) => {
         const { name, value } = e.target;
         dispatch({
-            type: 'WRITE_TODO',
+            type: "WRITE_TODO",
             name,
             value
         });
     }
 
-    const onClickList = () => {
+    const onClickAdd = () => {
         dispatch({
-            type : 'ADD_TODO',
+            type: "ADD_TODO",
             id: nextId.current,
             title: todoList.inputs.title,
             content: todoList.inputs.content
@@ -76,49 +71,51 @@ const UseReducer = () => {
         nextId.current += 1;
     }
 
+    const onClickDelete = (id) => {
+        dispatch({
+            type: "DELETE_TODO",
+            id
+        });
+    }
+
+    const onClickFix = (id) => {
+        const temp = prompt();
+        dispatch({
+            type: "FIX_TODO",
+            id,
+            temp
+        });
+    }
+
     const Show = ({ list }) => {
         return (
             <>
-                <div style={{ backgroundColor: "gray", color: "pink" }}>
-                    <div>{list.title} <br /> {list.content} </div>
-                    <button onClick={() => onRemove(list.id)}>삭제</button>
-                    <button onClick={() => onFix(list.id)}>수정</button>
+                <div style={{ backgroundColor: "pink" }}>
+                    <div>{list.title}</div>
+                    <div>{list.content}</div>
                 </div>
+                <button onClick={() => onClickDelete(list.id)}>삭제</button>
+                <button onClick={() => onClickFix(list.id)}>수정</button>
+                <br />
                 <br />
             </>
         );
     }
 
-    const onFix = (id) => {
-        const temp = prompt();
-        dispatch({
-            type : 'FIX_TODO',
-            temp,
-            id
-        });
-    }
-
-    const onRemove = (id) => {
-        dispatch({
-            type: 'DELETE_TODO',
-            id
-        });
-    }
-
     return (
         <div>
             <label>제목</label>
-            <input type="text" name="title" onChange={onChangeBox} />
+            <input type="text" name="title" onChange={onChangeTyping} value={todoList.list.title} />
             <br />
             <label>내용</label>
-            <input type="text" name="content" onChange={onChangeBox} />
+            <input type="text" name="content" onChange={onChangeTyping} value={todoList.list.content} />
             <br />
-            <button onClick={onClickList}>추가</button>
+            <button onClick={onClickAdd}>추가</button>
             <br />
-            <div>
-                {todos.map(list => (
-                    <Show list={list} key={list.id} />))}
-            </div>
+            <br />
+            {todoList.list.map(list => (
+                <Show list={list} id={list.id} />
+            ))}
         </div>
     );
 }
